79124d46c606783ac2bbcff0dbfa555845bb58e9
Oscar Cabanillas <oskbanillas@gmail.com>
2026-01-07 01:20:28 -0300

    [Added] CityCellViewModel

---
+++ CitySearchApp/Views/CityList/Cell/CityCellViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CityCellViewModel.swift
+//  CitySearchApp
+//
+//  Created by Oscar Cabanillas on 07/01/2026.
+//
+
+final class CityCellViewModel {
+    let city: CityModel
+    
+    var titleLabel: String {
+        return "\(city.name), \(city.countryCode)"
+    }
+    
+    var subtitleLabel: String {
+        return "Lat: \(city.coordinates.lat), Lon: \(city.coordinates.lon)"
+    }
+    
+    init(city: CityModel) {
+        self.city = city
+    }
+}
