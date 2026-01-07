1928c72c93fdf2575ffd83a894a2fe6885a5f1b9
Oscar Cabanillas <oskbanillas@gmail.com>
2026-01-07 01:26:32 -0300

    [Added] CityCellView

---
+++ CitySearchApp/Views/CityList/Cell/CityCellView.swift
@@ -0,0 +1,49 @@
+//
+//  CityCellView.swift
+//  CitySearchApp
+//
+//  Created by Oscar Cabanillas on 07/01/2026.
+//
+
+import SwiftUI
+
+struct CityCellView: View {
+    let viewModel: CityCellViewModel
+    
+    var body: some View {
+        HStack {
+            descriptionView
+            Spacer()
+            buttonsView
+        }
+    }
+    
+    private var descriptionView: some View {
+        VStack(alignment: .leading) {
+            Text(viewModel.titleLabel)
+                .font(.headline)
+            Text(viewModel.subtitleLabel)
+                .font(.subheadline)
+        }
+    }
+    
+    private var buttonsView: some View {
+        HStack {
+            Button(action: {}) {
+                Image(systemName: "info.circle")
+                    .foregroundColor(.blue)
+            }
+            Button(action: {}) {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+#Preview {
+    let city = CityModel(id: 1, name: "New York", countryCode: "US", coordinates: Coordinates(lat: 40.7128, lon: -74.0060), isFavorite: false)
+    CityCellView(
+        viewModel: CityCellViewModel(city: city)
+    )
+}
