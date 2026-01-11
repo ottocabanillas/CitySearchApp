252bba3577136a08ee7f0a007d9b927f22a9679d
Oscar Cabanillas <oskbanillas@gmail.com>
2026-01-11 02:01:22 -0300

    [Added] Ability to force error

--- CitySearchAppTests/Mocks/MockLocalCityStorage.swift
+++ CitySearchAppTests/Mocks/MockLocalCityStorage.swift
@@ -9,11 +9,30 @@ import Foundation
 @testable import CitySearchApp
 
 final class MockLocalCityStorage: CityStorage {
+    private var storage: Set<Int> = [2147714, 4829764]
+    var shouldThrowErrorOnLoad = false
+    var shouldThrowErrorOnSave = false
+    
     func load<T: Decodable>(_ type: T.Type) throws -> T {
-        <#code#>
+        if shouldThrowErrorOnLoad {
+            throw NSError(domain: "MockLocalCityStorage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Simulated load error"])
+        }
+        let data = try JSONEncoder().encode(storage)
+        return try JSONDecoder().decode(T.self, from: data)
     }
     
     func save<T: Encodable>(_ value: T) throws {
-        <#code#>
+        if shouldThrowErrorOnSave {
+            throw NSError(domain: "MockLocalCityStorage", code: 2, userInfo: [NSLocalizedDescriptionKey: "Simulated save error"])
+        }
+
+        let data = try JSONEncoder().encode(value)
+        if let newSet = try? JSONDecoder().decode(Set<Int>.self, from: data) {
+            storage = newSet
+        }
+    }
+    
+    func clear() {
+        storage.removeAll()
     }
 }
