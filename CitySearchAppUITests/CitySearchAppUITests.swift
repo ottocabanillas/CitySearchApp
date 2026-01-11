df504e6084177c0fc14cb2e358e4a1ec20bce53d
Oscar Cabanillas <oskbanillas@gmail.com>
2026-01-11 20:11:29 -0300

    [UITest] testFavoritesFilterFlow

--- CitySearchAppUITests/CitySearchAppUITests.swift
+++ CitySearchAppUITests/CitySearchAppUITests.swift
@@ -39,4 +39,30 @@ final class CitySearchAppUITests: XCTestCase {
         XCTAssertFalse(sydneyCell)
     }
     
+    func testFavoritesFilterFlow() throws {
+        //Given
+        let app = XCUIApplication()
+        app.launchArguments.append("--use-mock")
+        app.launch()
+        
+        //When
+        app/*@START_MENU_TOKEN@*/.switches["0"]/*[[".switches.switches[\"0\"]",".switches[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        
+        //Then
+        sleep(1)
+        
+        let alabamaCell = app.cells.containing(.staticText, identifier: "cityListView.cell_4829764").firstMatch.exists
+        let albuquerqueCell = app.cells.containing(.staticText, identifier: "cityListView.cell_5454711").firstMatch.exists
+        let anaheimCell = app.cells.containing(.staticText, identifier: "cityListView.cell_5323810").firstMatch.exists
+        let arizonaCell = app.cells.containing(.staticText, identifier: "cityListView.cell_5551752").firstMatch.exists
+        let sydneyCell = app.cells.containing(.staticText, identifier: "cityListView.cell_2147714").firstMatch.exists
+        
+        XCTAssertTrue(alabamaCell)
+        XCTAssertTrue(sydneyCell)
+        
+        XCTAssertFalse(albuquerqueCell)
+        XCTAssertFalse(anaheimCell)
+        XCTAssertFalse(arizonaCell)
+    }
+    
 }
