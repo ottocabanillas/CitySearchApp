df8e84b6bee04010613ef4242389c1c797e73794
Oscar Cabanillas <oskbanillas@gmail.com>
2026-01-12 10:16:44 -0300

    [Test] testIsPortraitLogic

--- CitySearchAppTests/OrientationObserverTests.swift
+++ CitySearchAppTests/OrientationObserverTests.swift
@@ -22,5 +22,16 @@ final class OrientationObserverTests: XCTestCase {
     }
     
     // MARK: - Tests
-    
+    func testIsPortraitLogic() {
+        //Given
+        //When
+        //Then
+        XCTAssertTrue(sut.isPortrait(for: .portrait), "Debe ser true para .portrait")
+        XCTAssertTrue(sut.isPortrait(for: .unknown), "Debe ser true para .unknown")
+        XCTAssertTrue(sut.isPortrait(for: .faceUp), "Debe ser false para .faceUp")
+        XCTAssertTrue(sut.isPortrait(for: .faceDown), "Debe ser false para .faceDown")
+        XCTAssertFalse(sut.isPortrait(for: .landscapeLeft), "Debe ser false para .landscapeLeft")
+        XCTAssertFalse(sut.isPortrait(for: .landscapeRight), "Debe ser false para .landscapeRight")
+        XCTAssertFalse(sut.isPortrait(for: .portraitUpsideDown), "Debe ser false para .portraitUpsideDown")
+    }
 }
